682590cec96ba920d0061654d4742229aeb31753
Rade58 <bajic.rade2@gmail.com>
2021-06-02 15:14:19 +0200

    🦉

--- pages/p/[siteId].tsx
+++ pages/p/[siteId].tsx
@@ -1,18 +1,59 @@
 /* eslint react/react-in-jsx-scope: 0 */
 /* eslint jsx-a11y/anchor-is-valid: 1 */
-import { FunctionComponent } from "react";
-import { GetStaticPaths, GetStaticProps } from "next";
+import type { FunctionComponent } from "react";
+import type { GetStaticPaths, GetStaticProps } from "next";
+import { useRef } from "react";
 import { useRouter } from "next/router";
 
-import { Box } from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 
-import { getAllSites, getAllFeedback } from "../../lib/db-admin";
+import { getAllSites, getAllFeedback } from "@/lib/db-admin";
+
+import { useAuth } from "@/lib/auth";
 
 interface FeedbackPagePropsI {
   initialFeedback: any;
 }
 
 type paramsType = { siteId: string };
+
+// PAGE
+// ------------------------------------------------------------
+const FeedbackPage: FunctionComponent<FeedbackPagePropsI> = ({
+  initialFeedback,
+}) => {
+  console.log({ initialFeedback });
+
+  // const { query } = useRouter();
+
+  const { user } = useAuth();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="full"
+      maxWidth="700px"
+      margin="0 auto"
+      border="1px solid crimson"
+    >
+      {user && (
+        <Box as="form">
+          <FormControl my={8}>
+            <FormLabel htmlFor="comment">Comment</FormLabel>
+            <Input ref={inputRef} id="comment" placeholder="Leave a comment" />
+            <Button type="submit" fontWeight="medium" mt={4}>
+              Add Comment
+            </Button>
+          </FormControl>
+        </Box>
+      )}
+    </Box>
+  );
+};
+// --------------------------------------------------------
 
 export const getStaticPaths: GetStaticPaths<paramsType> = async (ctx) => {
   const { sites } = await getAllSites();
@@ -44,16 +85,6 @@ export const getStaticProps: GetStaticProps<FeedbackPagePropsI, paramsType> =
       revalidate: 1,
     };
   };
-
-const FeedbackPage: FunctionComponent<FeedbackPagePropsI> = ({
-  initialFeedback,
-}) => {
-  console.log({ initialFeedback });
-
-  const { query } = useRouter();
-
-  return <Box>Page ID: {query.siteId}</Box>;
-};
 
 export default FeedbackPage;
 
